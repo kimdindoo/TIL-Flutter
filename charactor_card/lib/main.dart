2e2d47cf77340881d10a0ad61794995f3151447f
Jinsoo <aaa119o1125@gmail.com>
2022-10-12 00:18:33 +0900

    캐릭터 페이지 디자인

--- charactor_card/lib/main.dart
+++ charactor_card/lib/main.dart
@@ -8,35 +8,129 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Charactor card',
-      home: MyCard(),
+      debugShowCheckedModeBanner: false,
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyCard extends StatelessWidget {
-  const MyCard({Key? key}) : super(key: key);
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
         title: Text('BBANTO'),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.amber[700],
         elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Hello'),
-              Text('Hello'),
-              Text('Hello'),
-            ],
-          ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/penguin.gif'),
+                radius: 60.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('BBANTO',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
-
     );
   }
 }
