95554c59c27e8f0f80694ec5b1717666f216a2a2
kimdindoo <aaa119o1125@gmail.com>
2022-10-24 17:29:40 +0900

    web_view

---
+++ web_view/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:web_view_android/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
---
+++ web_view/lib/screen/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  final homeUrl = 'https://blog.codefactory.ai';
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('클릭!');
+              if(controller == null){
+                return;
+              }
+
+              controller!.loadUrl(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          )
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
