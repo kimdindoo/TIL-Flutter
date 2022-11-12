93e317b41886d3eb5e119642c9db264ede207cb2
Jinsoo <aaa119o1125@gmail.com>
2022-11-12 23:56:59 +0900

    FutureBuilder

---
+++ future_and_stream_builder/android/app/src/main/kotlin/com/example/future_and_stream_builder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.future_and_stream_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ future_and_stream_builder/lib/screen/home_screen.dart
@@ -0,0 +1,104 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 16.0,
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: getNumber(),
+          builder: (context, snapshot) {
+            // x
+            // if (snapshot.connectionState == ConnectionState.waiting) {
+            //   return Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // }
+            // o
+            // if(!snapshot.hasData){
+            //   return Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // }
+            if(snapshot.hasData){
+              // 데이터가 있을때 위젯 렌더링
+            }
+
+            if(snapshot.hasError){
+              // 에러가 났을때 위젯 렌더링
+            }
+
+            // 로딩중일때
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'FutureBuilder',
+                  style: textStyle.copyWith(
+                      fontWeight: FontWeight.w700, fontSize: 20.0),
+                ),
+                Text(
+                  'ConState : ${snapshot.connectionState}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Data : ${snapshot.data}',
+                  style: textStyle,
+                ),
+                // 특정 영역만 Indicator 보이게
+                // Row(
+                //   children: [
+                //     Text(
+                //       'Data : ${snapshot.data}',
+                //       style: textStyle,
+                //     ),
+                //     if(snapshot.connectionState == ConnectionState.waiting)
+                //       CircularProgressIndicator(
+                //
+                //       ),
+                //   ],
+                // ),
+                Text(
+                  'Error : ${snapshot.error}',
+                  style: textStyle,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text('setState'),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final random = Random();
+
+    // throw Exception('에러가 발생했습니다.');
+
+    return random.nextInt(100);
+  }
+}
