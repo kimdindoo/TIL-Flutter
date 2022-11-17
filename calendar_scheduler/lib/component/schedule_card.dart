9a279d697cc02afb83d0be4d85c7989e88602c25
김진수 <jskim@rootlink.co.kr>
2022-11-17 17:20:16 +0900

    calendar_scheduler

---
+++ calendar_scheduler/lib/component/schedule_card.dart
@@ -0,0 +1,123 @@
+import 'package:calender_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ScheduleCard extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+  final String content;
+  final Color color;
+
+  const ScheduleCard({
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IntrinsicHeight( // Row 안에 가장 높은 위치 위젯이랑 맞춰서 늘어남
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(
+                startTime: startTime,
+                endTime: endTime,
+              ),
+              SizedBox(width: 16.0),
+              _Content(
+                content: content,
+              ),
+              SizedBox(width: 16.0),
+              _Category(
+                color: color,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: PRIMARY_COLOR,
+      fontSize: 16.0,
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${startTime.toString().padLeft(2, '0')}:00',
+          style: textStyle,
+        ),
+        Text(
+          '${endTime.toString().padLeft(2, '0')}:00',
+          style: textStyle.copyWith(
+            fontSize: 10.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  final String content;
+
+  const _Content({
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        content,
+      ),
+    );
+  }
+}
+
+class _Category extends StatelessWidget {
+  final Color color;
+
+  const _Category({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      width: 16.0,
+      height: 16.0,
+    );
+  }
+}
--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,17 +1,100 @@
 import 'package:calender_scheduler/component/calendar.dart';
+import 'package:calender_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calender_scheduler/component/schedule_card.dart';
+import 'package:calender_scheduler/component/today_banner.dart';
+import 'package:calender_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
       body: SafeArea(
         child: Column(
           children: [
-            Calender(),
-          ]
+            Calender(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TodayBanner(
+              selectedDay: selectedDay,
+              scheduleCount: 3,
+            ),
+            SizedBox(height: 8.0),
+            _ScheduleList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true, // 모달 전체창으로 변환
+          builder: (_){
+            return ScheduleBottomSheet();
+          },
+        );
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(
+        Icons.add,
+      ),
+    );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    print(selectedDay);
+    setState(() {
+      this.selectedDay = selectedDay;
+      this.focusedDay = focusedDay;
+    });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+          itemCount: 10,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8.0);
+          },
+          itemBuilder: (context, index) {
+            return ScheduleCard(
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부하기.',
+              color: Colors.red,
+            );
+          },
         ),
       ),
     );
