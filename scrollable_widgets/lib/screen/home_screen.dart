934472003d6c0aa31da7f95fb638e200f26c8e99
Jinsoo <aaa119o1125@gmail.com>
2022-12-13 19:59:14 +0900

    scrollable_widgets

--- scrollable_widgets/lib/screen/home_screen.dart
+++ scrollable_widgets/lib/screen/home_screen.dart
@@ -3,7 +3,9 @@ import 'package:scrollable_widgets/layout/main_layout.dart';
 import 'package:scrollable_widgets/screen/custom_scroll_view_screen.dart';
 import 'package:scrollable_widgets/screen/grid_view_screen.dart';
 import 'package:scrollable_widgets/screen/list_view_screen.dart';
+import 'package:scrollable_widgets/screen/refresh_indicator.dart';
 import 'package:scrollable_widgets/screen/reorderable_list_view_screen.dart';
+import 'package:scrollable_widgets/screen/scrollbar_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll_view_screen.dart';
 
 class ScreenModel {
@@ -37,6 +39,14 @@ class HomeScreen extends StatelessWidget {
     ScreenModel(
       builder: (_) => CustomScrollViewScreen(),
       name: 'CustomScrollViewScreen',
+    ),
+    ScreenModel(
+      builder: (_) => ScrollbarScreen(),
+      name: 'ScrollbarScreen',
+    ),
+    ScreenModel(
+      builder: (_) => RefreshIndicatorScreen(),
+      name: 'RefreshIndicatorScreen',
     ),
   ];
 
